a317d7d76d8d8f1e310b713b17d8cefee99283e9
Kyle Mayer <x@kylemayer.dev>
2021-09-20 10:53:32 -0700

    get all test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Beer from '../lib/models/Beer.js';
+import Beer from '../lib/models/Beer.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -17,6 +17,18 @@ describe('demo routes', () => {
       id: '1',
       ...beer
     });
+  });
+
+  it('returns all beers via GET', async () => {
+    const beer1 = await Beer.insert({ name: 'beer1', style: 'pale', hops: 'simcoe' });
+    const beer2 = await Beer.insert({ name: 'beer2', style: 'ipa', hops: 'mosaic'  });
+    const beer3 = await Beer.insert({ name: 'beer3', style: 'double ipa', hops: 'vic secret' });
+
+    return request(app)
+      .get('/api/v1/beers')
+      .then((res) => {
+        expect(res.body).toEqual([beer1, beer2, beer3]);
+      });
   });
 
 
