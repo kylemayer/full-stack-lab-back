baefed2791d9325a5fdb63893fabef77d615ae3d
Kyle Mayer <x@kylemayer.dev>
2021-09-20 10:37:36 -0700

    post test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,22 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+// import Beer from '../lib/models/Beer.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  afterAll(() => {
-    pool.end();
+  it('creates a single beer via POST', async () => {
+    const beer = { name: 'beer', style: 'pilsner', hops: 'noble' };
+    const res = await request(app).post('/api/v1/beers').send(beer);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...beer
+    });
   });
+
+
 });
